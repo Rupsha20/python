0c052ee72480c852e9f590cb9cd77a8e17cf69e4
Rupsha20 <rupsha049@gmail.com>
2024-11-25 01:46:39 +0530

    Hill_climbing.py

--- Hill_climbing.py
+++ Hill_climbing.py
@@ -1,13 +1,10 @@
 import random
 
-# Define the goal state and blank tile
 GOAL_STATE = [[1, 2, 3], [4, 5, 6], [7, 8, 'B']]
 
-# Function to check if current state is goal state
 def is_goal(state):
     return state == GOAL_STATE
 
-# Heuristic h1: Number of displaced tiles
 def heuristic_displaced(state):
     displaced = 0
     for i in range(3):
@@ -16,7 +13,6 @@ def heuristic_displaced(state):
                 displaced += 1
     return displaced
 
-# Heuristic h2: Total Manhattan distance
 def heuristic_manhattan(state):
     distance = 0
     for i in range(3):
@@ -25,15 +21,13 @@ def heuristic_manhattan(state):
                 x, y = divmod(state[i][j] - 1, 3)
                 distance += abs(i - x) + abs(j - y)
     return distance
-
-# Function to get the position of the blank tile
+    
 def find_blank(state):
     for i in range(3):
         for j in range(3):
             if state[i][j] == 'B':
                 return i, j
 
-# Generate possible moves (up, down, left, right)
 def get_neighbors(state):
     neighbors = []
     x, y = find_blank(state)
@@ -46,7 +40,6 @@ def get_neighbors(state):
             neighbors.append(new_state)
     return neighbors
 
-# Hill climbing algorithm
 def hill_climbing(start_state, heuristic):
     current = start_state
     while True:
@@ -68,18 +61,15 @@ def hill_climbing(start_state, heuristic):
 
         current = next_state
 
-# Function to print the puzzle state
 def print_state(state):
     for row in state:
         print(' '.join(str(cell) for cell in row))
     print()
 
-# Example usage
 start_state = [[1, 2, 3], [4, 6, 5], [7, 'B', 8]]
 
 print("Start State:")
 print_state(start_state)
-#print("Using Heuristic h1 (Number of Displaced Tiles):")
 solution = hill_climbing(start_state, heuristic_displaced)
 print("Solution State with h1 (Misplaced Tiles):")
 print_state(solution)
